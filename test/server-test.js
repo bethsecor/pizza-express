13c0a671cdb12a0cedd0ac8f14a2af592e6650d2
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-16 19:53:09 -0600

    Test for ok status

--- test/server-test.js
+++ test/server-test.js
@@ -1,11 +1,16 @@
 const assert = require('assert');
 const app = require('../server');
+const request = require('request');
 
 before(done => {
   this.port = 9876;
   this.server = app.listen(this.port, (err, result) => {
     if (err) { return done(err); }
     done();
+  });
+
+  this.request = request.defaults({
+    baseUrl: 'http://localhost:9876/'
   });
 });
 
@@ -17,6 +22,16 @@ describe('Server', () => {
 
   it('should exist', () => {
     assert(app);
+  });
+
+  describe('GET /', () => {
+    it('should return a 200', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
   });
 
 });
